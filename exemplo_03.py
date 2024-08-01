fad16df069e81b025f7c41e4302b71206041b215
william oliveira <will.h.o@hotmail.com>
2024-07-31 23:20:45 -0300

    exemplo 3

---
+++ exemplo_03.py
@@ -0,0 +1,9 @@
+
+constante = 1000
+nome = str(input("Digite seu nome \n"))
+salario = float(input("Digite seu salario: \n"))
+bonus = float(input("Digite o valor do bonus \n"))
+
+calculo = constante + salario * bonus 
+
+print(f"O usuario {nome}, tem um bonus de {calculo} ")
